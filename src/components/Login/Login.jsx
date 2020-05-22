8757a4b200ce551ec62382a0bce39581d57250a2
t.manikanta reddy <thikkavarapureddy@ninjacart.com>
2020-05-22 22:40:05 +0530

    FirstChange

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <div className="container-fluid my-container">
+                <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    </div>
+                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center">
+                        <br /><br /><br /><br />
+                        <div className="login">
+                            <h5 style={{color: "#2867B2"}}><i className="fa fa-rocket"></i>Communi<span style={{color:"black"}}>cation</span></h5>
+                            <h6>Login</h6><br />
+
+                        </div>
+                    </div>
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
