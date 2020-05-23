aaf5e6d41e002528079be89e05e893d4241e46a4
t.manikanta reddy <thikkavarapureddy@ninjacart.com>
2020-05-23 14:56:34 +0530

    Changes-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import {BrowserRouter,Route, Redirect} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Login />
+      <BrowserRouter>
+      <Route exact path="/">
+      <Redirect to="/login" />
+      </Route>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/home" component={Home} />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import "./Login.css";
 
 function Login() {
-    return (
-        <div>
-            <div className="container-fluid my-container">
-                <div className="row">
-                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center">
-                        <br /><br /><br /><br />
-                        <div className="login">
-                            <h5 style={{color: "#2867B2"}}><i className="fa fa-rocket"></i>Communi<span style={{color:"black"}}>cation</span></h5>
-                            <h6>Login</h6><br />
 
-                        </div>
+    const [Username, setUsername] = useState('');
+    const [Password, setPassword] = useState('');
+
+    const mySubmitHandler = () => {
+        console.log(Username,Password);
+    }
+    return (
+        <div className="container-fluid my-container">
+            <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center">
+                    <br /><br /><br /><br />
+                    <div className="login">
+                        <h5 style={{ color: "#2867B2" }}><i className="fa fa-rocket"></i>Communi<span style={{ color: "black" }}>cation</span></h5>
+                        <h6>Login</h6><br />
+                        <form onSubmit={mySubmitHandler()}>
+                        <input type="text" value={Username} onChange={e => setUsername(e.target.value)} /><br/>
+                        <input type="password" value={Password} onChange={e => setPassword(e.target.value)} /><br/><br/>
+                        <button className="button button1" type="button" disabled={Username == '' || Password == ''}>Submit</button>
+                        </form>
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4">
-                    </div>
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4">
                 </div>
             </div>
         </div>
